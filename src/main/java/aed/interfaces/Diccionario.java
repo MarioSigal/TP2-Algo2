ddc3a25b90d33aa5f933f32957991a67f1b2666c
MarioSigal <116769855+MarioSigal@users.noreply.github.com>
2024-06-16 17:01:14 -0300

    actualizo interfaces

---
+++ src/main/java/aed/interfaces/Diccionario.java
@@ -0,0 +1,42 @@
+package aed.interfaces;
+
+public interface Diccionario<T>{
+
+    /**
+     * Define una nueva calve, con su valor asignado.
+     * 
+     */
+    public void definir(String clave,T valor);
+    
+    /**
+     * Devuelve si la clave se encuentra definida.
+     * 
+     */
+    public boolean esta(String clave);
+    
+    /**
+     * Devuelve el valor de una clave.
+     * 
+     */
+    public T obtener(String clave);
+    
+    /**
+     * Devuelve la cantidad de claves en el diccionario.
+     * 
+     */
+    public int longitud();
+    
+    /**
+     * Elimina una clave del diccionario.
+     * 
+     */
+    public void borrar(String clave);
+    
+    /**
+     * Devuelve ListaEnlazada<String> con las claves del diccionario en orden lexicografico.
+     * 
+     */
+    public ListaEnlazada<String> obtenerElems();
+
+
+} 
