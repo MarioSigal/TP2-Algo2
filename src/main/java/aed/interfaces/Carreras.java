5aad00e46274919eaef64f41cca586772e7607a3
MarioSigal <116769855+MarioSigal@users.noreply.github.com>
2024-06-18 18:37:39 -0300

    agrego complejidades de CarrerasImpl a Carreras.java

--- src/main/java/aed/interfaces/Carreras.java
+++ src/main/java/aed/interfaces/Carreras.java
@@ -5,24 +5,29 @@ import aed.MateriasImpl;
 public interface Carreras{
     /**
      * Crea una nueva carrera asignandole sus materias.
-     * 
+     * Complejidades:
+     *  -CarrerasImpl: O(long(c)), donde c es la carrera a agregar.
      */
     public void agregarCarrera(String nombreCarrera, MateriasImpl trieMaterias);
     
     /**
-     * Devuelve el diccionario de materias de una carrera.
-     *
+     * Devuelve diccionario de materias de una carrera.
+     * Complejidades:
+     *  -CarrerasImpl: O(long(c)), donde c es la carrera a obtener.
      */
     public Materias obtenerMateriasCarrera(String nombreCarrera);
     
     /**
      * Devuelve una String[] con los nombres de las carreras existentes en orden lexicografico.
-     * 
+     * Complejidades:
+     *  -CarrerasImpl:O(sum_{ceC}{long(c)}), donde C es el conjunto de Carreras registradas.
      */
     public String[] obtenerListaDeCarreras();
-    /**s
+    
+    /**
      * Devuelve si la Carrera se encuentra definida.
-     * 
+     * Complejidades:
+     *  -CarrerasImpl: O(long(c)), donde c es la carrera a buscar.
      */
     public boolean estaRegistrada(String nombreCarrera);
 }
