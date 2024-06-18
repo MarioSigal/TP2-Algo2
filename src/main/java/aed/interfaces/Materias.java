dde4b0c2129ab8070fd035ef73e8cfb325f49fd3
MarioSigal <116769855+MarioSigal@users.noreply.github.com>
2024-06-18 15:51:00 -0300

    agregue interfaz Materias.java

--- src/main/java/aed/interfaces/Materias.java
+++ src/main/java/aed/interfaces/Materias.java
@@ -1,5 +1,21 @@
 package aed.interfaces;
 
 public interface Materias {
+    /**
+     * Crea una nueva materia con su informacion correspondiente.
+     * 
+     */
+    public void agregarMateria(String nombreMateria, DataMateria data);
     
+    /**
+     * Devuelve la informacion correspondiente a una materia.
+     * 
+     */
+    public DataMateria obtenerMateria(String nombreMateria);
+    
+    /**
+     * Devuelve una String[] con los nombres de las materias existentes en orden lexicografico.
+     * 
+     */
+    public String[] obtenerListaDeMaterias();
 }
