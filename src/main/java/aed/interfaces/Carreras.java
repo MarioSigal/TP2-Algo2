146d250fadbb8d0451856b053a19499815554f28
MarioSigal <116769855+MarioSigal@users.noreply.github.com>
2024-06-18 15:49:05 -0300

    agregue la interfaz Carreras.java

--- src/main/java/aed/interfaces/Carreras.java
+++ src/main/java/aed/interfaces/Carreras.java
@@ -1,5 +1,26 @@
 package aed.interfaces;
 
-public interface Carreras {
+public interface Carreras{
+    /**
+     * Crea una nueva carrera asignandole sus materias.
+     * 
+     */
+    public void agregarCarrera(String nombreCarrera, Materias trieMaterias);
     
+    /**
+     * Devuelve el diccionario de materias de una carrera.
+     *
+     */
+    public Materias obtenerCarrera(String nombreCarrera);
+    
+    /**
+     * Devuelve una String[] con los nombres de las carreras existentes en orden lexicografico.
+     * 
+     */
+    public String[] obtenesListaDeCarreras();
+    /**s
+     * Devuelve si la Carrera se encuentra definida.
+     * 
+     */
+    public boolean estaRegistrada(String nombreCarrera);
 }
