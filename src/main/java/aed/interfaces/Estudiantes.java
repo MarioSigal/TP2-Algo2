4c83b219392d56d64041886977143a4c73db4d97
MarioSigal <116769855+MarioSigal@users.noreply.github.com>
2024-06-18 19:05:17 -0300

    Agrego complejidades a Estudiantes.java

--- src/main/java/aed/interfaces/Estudiantes.java
+++ src/main/java/aed/interfaces/Estudiantes.java
@@ -3,24 +3,28 @@ package aed.interfaces;
 public interface Estudiantes {
     /**
      * Registra el ingreso de un/a nuevo/a estudiante al sistema de la universidad e inicializa su cantidad de materias inscriptas en 0.
-     * 
+     * Complejidades:
+     *  -EstudiantesImpl: O(long(e)) = O(1) porque long(e) es acotada, donde e es la LU de el/la estudiante a registrar.
      */
     public void registrarEnelSistema(String lu);
     
     /**
      * Amenta la cantidad de materias inscriptas de el/la estudiante en 1.
-     * 
+     * Complejidades:
+     *  -EstdiantesImpl: O(long(e)) = O(1) porque long(e) es acotada, donde e es la LU de el/la estudiante a inscribir.
      */
     public void inscribirAMateria(String lu);
     
     /**
      * Devuelve la cantidad de materias en las que esta inscripto/a un/a estudiante.
-     * 
+     * Complejidades:
+     *  -EstdiantesImpl: O(long(e)) = O(1) porque long(e) es acotada, donde e es la LU de el/la estudiante al cual se refiere.
      */
     public int cantMateriasInscriptas(String lu);
     /**
      * Reduce la cantidad de materias inscriptas de el/la estudiante en 1.
-     * 
+     * Complejidades:
+     *  -EstdiantesImpl: O(long(e)) = O(1) porque long(e) es acotada, donde e es la LU de el/la estudiante a desinscribir.
      */
     public void desinscribir(String lu);   
 }
