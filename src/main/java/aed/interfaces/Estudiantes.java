0a96053b0735e8b5d8c1a3d82644944fdaeda680
MarioSigal <116769855+MarioSigal@users.noreply.github.com>
2024-06-18 17:14:34 -0300

    agregue Estudiantes.java

--- src/main/java/aed/interfaces/Estudiantes.java
+++ src/main/java/aed/interfaces/Estudiantes.java
@@ -1,5 +1,26 @@
 package aed.interfaces;
 
 public interface Estudiantes {
+    /**
+     * Registra el ingreso de un/a nuevo/a estudiante al sistema de la universidad e inicializa su cantidad de materias inscriptas en 0.
+     * 
+     */
+    public void registrarEnelSistema(String lu);
     
+    /**
+     * Amenta la cantidad de materias inscriptas de el/la estudiante en 1.
+     * 
+     */
+    public void inscribirAMateria(String lu);
+    
+    /**
+     * Devuelve la cantidad de materias en las que esta inscripto/a un/a estudiante.
+     * 
+     */
+    public int cantMateriasInscriptas(String lu);
+    /**
+     * Reduce la cantidad de materias inscriptas de el/la estudiante en 1.
+     * 
+     */
+    public void desinscribir(String lu);   
 }
