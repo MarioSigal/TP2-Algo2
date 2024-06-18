a86e7e7a337885c7a33f7c19a581e899cc2d8e83
MarioSigal <116769855+MarioSigal@users.noreply.github.com>
2024-06-18 18:47:18 -0300

    agregue complejidades a Materias.java

--- src/main/java/aed/interfaces/Materias.java
+++ src/main/java/aed/interfaces/Materias.java
@@ -3,21 +3,24 @@ package aed.interfaces;
 import aed.DataMateria;
 
 public interface Materias {
-    /**
+   /**
      * Crea una nueva materia con su informacion correspondiente.
-     * 
+     * Complejidades:
+     *  -MateriasImpl: O(long(m)), donde m es la materia a agregar.
      */
     public void agregarMateria(String nombreMateria, DataMateria data);
     
     /**
      * Devuelve la informacion correspondiente a una materia.
-     * 
+     * Complejidades:
+     *  -MateriasImpl: O(long(m)), donde m es la materia de la cual se obtiene la informacion.
      */
     public DataMateria obtenerMateria(String nombreMateria);
     
     /**
      * Devuelve una String[] con los nombres de las materias existentes en orden lexicografico.
-     * 
+     * Complejidades:
+     *  -MateriasImpl:O(sum_{meM}{long(m)}), donde M es el conjunto de Materias registradas.
      */
     public String[] obtenerListaDeMaterias();
 }
