4bc501d0e7055abab9a960c869e34976dd51daf8
MarioSigal <116769855+MarioSigal@users.noreply.github.com>
2024-06-19 13:26:08 -0300

    Add files via upload

---
+++ src/main/java/aed/interfaces/DatosMateria.java
@@ -0,0 +1,67 @@
+package aed.interfaces;
+
+public interface DatosMateria{
+    /**
+     * Registra a el/la estudiante en la materia.
+     * Complejidad:
+     *  -DataMateria:O(1)
+     */
+    public void agregarAlumno(String lu);
+    /**
+     * Registra nombres de la materia en otras carreras.
+     * Complejidad:
+     *  -DataMateria:O(1)
+     */
+    public void agregarNombre(String nombreMateria);
+    
+    /**
+     * Registra los punteros a la posicion en memoria de los MateriasImpl de otras carreras que tienen una misma materia
+     * Complejidad:
+     *  -DataMateria:O(1)
+     */
+    public void agregarTrie(MateriasImpl trieMateria);
+    
+    /**
+     * Devuelve con int[] con el plantel Docente de esta materia [#PROF, #JTP, #AY1, #AY2]
+     * Complejidad:
+     *  -DataMateria:O(1)
+     */
+    public int[] obtenerPlantel ();
+    
+    /**
+     * Se agrega a el/la Docente al conteo de su cargo asignado
+     * Complejidad:
+     *  -DataMateria:O(1)
+     */
+    public void agregarDocente(CargoDocente cargo);
+    
+    /**
+     * Devuelve la cantidad de estudiantes inscriptos en la materia.
+     * Complejidad:
+     *  -DataMateria:O(1)
+     */
+    public int cantInscriptos();
+    
+    /**
+     * Devuelve una lista con las LU de los estudiantes inscriptos en la materia.
+     * Complejidad:
+     *  -DataMateria:O(1)
+     */
+    public ListaEnlazada<String> obtenerInscriptos();
+    
+    /**
+     * Borra esta materia en todas las carreras en las que se encuentra disponible.
+     * Complejidad:
+     *  -DataMateria: O(sum_{neN_{m}}{long(n)})
+     */
+    public void cerrarMateria();
+    
+    /**
+     * Devuelve si los estdiantes inscriptos exceden o no el cupo
+     * Complejidad:
+     *  -DataMateria: O(1)
+     */
+    public boolean excedeCupo();
+
+
+}
